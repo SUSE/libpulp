9b8c56d854f8887d5e5aafd5ead687d04b900d3c
Joao Moreira <jmoreira@suse.de>
2018-09-11 14:39:33 -0300

    Improve rerun.sh script

--- rerun.sh
+++ rerun.sh
@@ -6,9 +6,18 @@ rpmbuild -bb libpulp.spec
 sudo rpm -ivh rpmbuild/rpms/x86_64/libpulp-0.1-0.x86_64.rpm
 sudo rpm -ivh rpmbuild/rpms/x86_64/dummyapp-0.1-0.x86_64.rpm
 rpmbuild -bb livepatch.spec
-cp /tmp/patch_test.so /tmp/patch1.so
 #s/dummyapp1/dummyappX/ to test different apps
 LD_PRELOAD=/usr/lib64/libpulp.so /usr/bin/dummyapp1 &
 sleep 3
 sudo rpm -ivh rpmbuild/rpms/x86_64/libdummy_livepatch-0.1-0.x86_64.rpm
+sleep 3
+sudo rpm -e libdummy_livepatch-0.1-0.x86_64.rpm
+sleep 1
+LD_PRELOAD=/usr/lib64/libpulp.so /usr/bin/dummyapp3 &
+sudo rpm -ivh rpmbuild/rpms/x86_64/libdummy_livepatch-0.1-0.x86_64.rpm
 sleep 5
+lua /usr/bin/dispatcher /var/ulp/libdummy_livepatch-0.1/metadata.ulp
+sleep 4
+killall dummyapp1
+killall dummyapp3
+sleep 2
