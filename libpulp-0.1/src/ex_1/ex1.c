3ba4ea630c7f19eebb69cc20379b07f8bce60e98
Joao Moreira <jmoreira@suse.de>
2018-09-21 12:42:18 +0200

    Improve verbosity of ex1 and trigger tool

--- libpulp-0.1/src/ex_1/ex1.c
+++ libpulp-0.1/src/ex_1/ex1.c
@@ -4,6 +4,7 @@
 int main() {
     fprintf(stderr, "dummyapp: Hello there :))\n");
     foo(1);
-    while (bar()) { sleep(1); }
+    fprintf(stderr, "THIS IS EX1: ");
+    while (bar()) { sleep(1); fprintf(stderr, "THIS IS EX1: ");}
     foo(2);
 }
