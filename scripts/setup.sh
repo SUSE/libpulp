350a61c2d810e1a973056991131c66ebc312e99a
Joao Moreira <jmoreira@suse.de>
2018-10-23 15:21:09 +0200

    Improve directory structure
    move scripts into its own directory
    remove .tar.bz2 file

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,26 @@
 #!/bin/sh
+
+# make sure we are in the right directory
+RUN_DIR=$(basename $(pwd))
+
+if [ $RUN_DIR == "scripts" ];
+then
+	cd ..
+	RUN_DIR=$(basename $(pwd))
+fi
+
+if [ $RUN_DIR != "libpulp" ];
+then
+	echo "Please, run this from libpulp root directory"
+	exit
+fi
+
+# uninstall old packages, for when retrying
 sudo rpm -e libdummy_livepatch-0.1-0.x86_64
 sudo rpm -e libpulp-0.1-0.x86_64
 sudo rpm -e dummyapp-0.1-0.x86_64
+
+# build and install libpulp and examples
 rpmbuild -bb libpulp.spec
 sudo rpm -ivh rpmbuild/rpms/x86_64/libpulp-0.1-0.x86_64.rpm
 sudo rpm -ivh rpmbuild/rpms/x86_64/dummyapp-0.1-0.x86_64.rpm
