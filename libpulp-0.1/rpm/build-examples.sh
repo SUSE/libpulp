6132c441c8345a3723e7b14ec79290ebb2a0cb8e
Joao Moreira <jmoreira@suse.de>
2019-03-08 13:26:18 -0300

    Move examples into dummy src directory
    Signed-off-by: Joao Moreira <jmoreira@suse.de>
    [nstange@suse.de: massage diff to make it apply on top of the cleaned git
                      history]
    Signed-off-by: Nicolai Stange <nstange@suse.de>

--- libpulp-0.1/rpm/build-examples.sh
+++ libpulp-0.1/rpm/build-examples.sh
@@ -23,18 +23,18 @@ echo $'** Building libpulp'
 
 echo $'** Building libdummy'
 pwd
-${CC} -c ../src/libdummy/libdummy.c -o ./build/libdummy.o -Wall -fPIC -fpatchable-function-entry=${NOP_LENGTH}
+${CC} -c ../src/dummy/libdummy/libdummy.c -o ./build/libdummy.o -Wall -fPIC -fpatchable-function-entry=${NOP_LENGTH}
 # weak is built just to show that modified binutils support weak symbols
-${CC} -c ../src/libdummy/weak.c -o ./build/weak.o -Wall -fPIC -fpatchable-function-entry=${NOP_LENGTH}
+${CC} -c ../src/dummy/libdummy/weak.c -o ./build/weak.o -Wall -fPIC -fpatchable-function-entry=${NOP_LENGTH}
 ${ULP_LD} -shared -o build/libdummy.so build/libdummy.o build/weak.o build/trm.o -lc --build-id
 
 echo $'\n** Adjusting .dynsym and .ulp'
 ./build/dynsym_gate build/libdummy.so
 
 echo $'** Building examples'
-${CC} -c ../src/ex_1/ex1.c -o ./build/ex1.o
-${CC} -c ../src/ex_2/ex2.c -o ./build/ex2.o
-${CC} -c ../src/ex_3/ex3.c -o ./build/ex3.o
+${CC} -c ../src/dummy/ex_1/ex1.c -o ./build/ex1.o
+${CC} -c ../src/dummy/ex_2/ex2.c -o ./build/ex2.o
+${CC} -c ../src/dummy/ex_3/ex3.c -o ./build/ex3.o
 ${LD} -dynamic-linker ${LINKER} ${LIB}crt1.o ${LIB}crti.o ./build/ex1.o ${LIB}crtn.o -lc -ldl -ldummy -Lbuild --build-id -o ./build/ex1
 ${LD} -dynamic-linker ${LINKER} ${LIB}crt1.o ${LIB}crti.o ./build/ex2.o ${LIB}crtn.o -lc -ldl -lpthread -ldummy -Lbuild --build-id -o ./build/ex2
 ${LD} -dynamic-linker ${LINKER} ${LIB}crt1.o ${LIB}crti.o ./build/ex3.o ${LIB}crtn.o -lc -ldl -ldummy -Lbuild --build-id -o ./build/ex3
