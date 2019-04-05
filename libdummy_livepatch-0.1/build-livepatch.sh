21b2de4880a501a5dea4230aaa02363cdbdb5566
Joao Moreira <jmoreira@suse.de>
2019-04-05 00:38:15 -0300

    Switch livepatch.spec to libdummy_livepatch.spec

--- libdummy_livepatch-0.1/build-livepatch.sh
+++ libdummy_livepatch-0.1/build-livepatch.sh
@@ -8,8 +8,7 @@ echo $'** Building livepatch for example'
 mkdir ./build
 ${CC} ./src/patch/patch.c -shared -o ./build/patch1.so -fPIC -Wall
 
-/usr/bin/ulp_packer ./src/patch/descr.txt /usr/lib64/libdummy.so
+/usr/bin/ulp_packer ./src/patch/descr.txt /usr/lib64/ulp_glibc/lib/libdummy.so
 mv metadata.ulp build/metadata1.ulp
 
-echo $'**Make sure libdummy.so is in /usr/lib64'
 echo $'\n** DONE'
