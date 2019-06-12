9191118c69dc2c1896d1c17de8e111a26092f54c
Joao Moreira <jmoreira@suse.de>
2019-06-12 12:16:33 -0300

    Fix rerun.sh LD_PRELOAD path
    Signed-off-by: Joao Moreira <jmoreira@suse.de>
    [nstange@suse.de: massage diff hunk to make it apply on top of the
                      cleaned up git history.]
    Signed-off-by: Nicolai Stange <nstange@suse.de>

--- scripts/rerun.sh
+++ scripts/rerun.sh
@@ -20,7 +20,7 @@ sudo rpm -e libdummy_livepatch-0.1-0.x86_64
 
 # run example
 # s/dummyapp1/dummyappX/ to test different apps
-LD_PRELOAD=/usr/lib64/libpulp.so /usr/bin/dummyapp1 &
+LD_PRELOAD=/usr/ulp/libpulp/libpulp.so /usr/bin/dummyapp1 &
 sleep 3
 #sudo rpm -ivh rpmbuild/rpms/x86_64/libdummy_livepatch-0.1-0.x86_64.rpm
 killall dummyapp1
