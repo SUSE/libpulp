cd340ccbc29799ba24215ddcf459aa2fb74976e8
Gabriel F. T. Gomes <gagomes@suse.de>
2020-05-22 15:02:57 -0300

    Remove hack to cause deadlock from the deadlock test
    The deadlock test case (tests/deadlock.c) has a signal handler that
    intentionally calls calloc. The test wrapper (tests/deadlock.py)
    triggers this signal handler, in a loop, to demonstrate the deadlock
    even if live patch installation luckily succeeded (the deadlock is not
    always reached during live patch installation).
    
    This patch replaces this demonstration hack with an outer loop of live
    patch installations, which increases the chance of hitting a deadlock
    during live patch installation. The loop iterates a fixed (hardcoded)
    number of times, which is large enough to always reproduce the problem
    (at least according to the tests I made).

--- tests/deadlock.c
+++ tests/deadlock.c
@@ -1,9 +1,6 @@
-#include <errno.h>
 #include <pthread.h>
-#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 
 /* At least one thread is required to cause the deadlock, otherwise,
@@ -22,29 +19,9 @@
 /* Do not optimize, otherwise the calloc/free sequences go away. */
 #pragma GCC optimize ("O0")
 
-void
-handler (int sig,
-         siginfo_t *info __attribute__ ((unused)),
-         void *ucontext __attribute__ ((unused)))
-{
-  char *data;
-
-  /* Should not call calloc (AS-Unsafe) from a signal handler. */
-  if (sig == SIGUSR1) {
-    printf ("handler entry\n");
-    fflush (stdout);
-    data = calloc (100, sizeof(char));
-    free (data);
-    printf ("handler exit\n");
-    fflush (stdout);
-  }
-}
-
 void *
 worker (void *arg __attribute__ ((unused)))
 {
-  char *data;
-
   while (1) {
     pause ();
   }
@@ -54,25 +31,10 @@ int
 main (void)
 {
   char *data;
-  struct sigaction act;
   pthread_t thread[THREADS];
-  sigset_t set;
 
-  memset(&act, 0, sizeof(act));
-  act.sa_sigaction = handler;
-  act.sa_flags = SA_SIGINFO;
-  errno = 0;
-  if (sigaction(SIGUSR1, &act, NULL)) {
-    perror("sigaction:");
-    return 1;
-  }
-
-  sigemptyset (&set);
-  sigaddset (&set, SIGUSR1);
-  pthread_sigmask (SIG_BLOCK, &set, NULL);
   for (int iter = 0; iter < THREADS; iter++)
     pthread_create (&thread[iter], NULL, worker, NULL);
-  pthread_sigmask (SIG_UNBLOCK, &set, NULL);
 
   /* Simple call to the target library, not necessary to demonstrate the
    * deadlock per se, but required to make this application depend on
