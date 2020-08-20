be42736a8469e6cc668b164f8522cd727f53c92c
Gabriel F. T. Gomes <gagomes@suse.de>
2020-08-20 18:31:56 -0300

    Relicense under LGPL version 2.1 or later
    GPL version 3 is incompatible with common free software licenses, such
    as with the OpenSSL license [1]. This raises concerns about the
    legality of linking trm.o with libcrypto or libssl, even though
    libpulp.so is never directly linked against them, but LD_PRELOAD'ed.
    
    This patch relicenses the whole of libpulp under LGPL version 2.1 or, at
    the option of redistributors, any later version. Alternatively,
    relicensing only trm.S could fix the compatibility problem.
    
    This patch was created by a set of scripts, which, apart from changing
    the license, update copyright years and add copyright notices to files
    that lacked it. The license file was downloaded from gnu.org [2].
    
    [1] https://www.gnu.org/licenses/license-list.en.html#OpenSSL
    [2] https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt

--- lib/ulp.c
+++ lib/ulp.c
@@ -5,20 +5,18 @@
  *
  *  This file is part of libpulp.
  *
- *  libpulp is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ *  libpulp is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
  *
  *  libpulp is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
  *  along with libpulp.  If not, see <http://www.gnu.org/licenses/>.
- *
- *  Author: Joao Moreira <jmoreira@suse.de>
  */
 
 #define _GNU_SOURCE
