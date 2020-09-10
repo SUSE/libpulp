592836780442075fafd85226d703e45e4b89c5e9
Gabriel F. T. Gomes <gagomes@suse.de>
2020-09-10 19:46:23 -0300

    Add test to demonstrate AS-Unsafe conversion
    A program may call AS-Safe functions in signal handlers, however, if
    that function gets live-patched, its prologue will make a call into
    libulp's universe handling routine, which calls dlsym, thus rendering
    the live-patched function AS-Unsafe. This means that the live-patched
    function can no longer be called from the signal handler, otherwise it
    might deadlock. This patch adds a test case that demonstrates this
    problem.
    
    Since the problem happens because of a call to dlsym, the test case
    relies on the repetitive use of functions from the dlopen family.
    However, this might cause an earlier bug to be exposed, which is more
    frequently detected by another test case (tests/deadlock.py). When that
    happens, this new test case returns a SKIP code, because it was unable
    to detect the AS-Unsafe conversion.

---
+++ tests/asunsafe_conversion.c
@@ -0,0 +1,67 @@
+/*
+ *  libpulp - User-space Livepatching Library
+ *
+ *  Copyright (C) 2020 SUSE Software Solutions GmbH
+ *
+ *  This file is part of libpulp.
+ *
+ *  libpulp is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  libpulp is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with libpulp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <dlfcn.h>
+#include <errno.h>
+#include <stdio.h>
+#include <signal.h>
+#include <string.h>
+
+#include <libblocked.h>
+
+void
+handler (int sig __attribute__ ((unused)),
+         siginfo_t *info __attribute__ ((unused)),
+         void *ucontext __attribute__ ((unused)))
+{
+  hello ();
+}
+
+int
+main (void)
+{
+  void *handle;
+  struct sigaction act;
+
+  /* Register handler for SIGUSR1. */
+  memset(&act, 0, sizeof(act));
+  act.sa_sigaction = handler;
+  act.sa_flags = SA_SIGINFO;
+  errno = 0;
+  if (sigaction(SIGUSR1, &act, NULL)) {
+    perror("sigaction:");
+    return 1;
+  }
+
+  /* Signal readiness. */
+  printf ("Waiting for signals.\n");
+
+  /* Wait for signals. */
+  while (1) {
+    /* Call dlopen functions in loop to make their locks busy. */
+    handle = dlopen (NULL, RTLD_LAZY | RTLD_NOLOAD);
+    if (handle != NULL) {
+      dlclose (handle);
+    }
+  }
+
+  return 0;
+}
---
+++ tests/asunsafe_conversion.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+#   libpulp - User-space Livepatching Library
+#
+#   Copyright (C) 2020 SUSE Software Solutions GmbH
+#
+#   This file is part of libpulp.
+#
+#   libpulp is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU Lesser General Public
+#   License as published by the Free Software Foundation; either
+#   version 2.1 of the License, or (at your option) any later version.
+#
+#   libpulp is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with libpulp.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import pexpect
+import re
+import signal
+import subprocess
+import sys
+
+# ULP tools location
+builddir = os.getcwd()
+trigger = builddir + '/../tools/trigger/ulp_trigger'
+preload = {'LD_PRELOAD': builddir + '/../lib/.libs/libpulp.so'}
+
+# Test case name and live patch selection variable
+testname = os.path.splitext(sys.argv[0])
+testname = os.path.basename(testname[0])
+
+# Start the test program and check default behavior
+child = pexpect.spawn('./' + testname, timeout=1, env=preload,
+                      encoding='utf-8')
+
+# Wait for the test program to be ready
+child.expect('Waiting for signals.\r\n')
+
+# Check default output
+print('Testing output before live patch... ', end='')
+child.kill(signal.SIGUSR1)
+child.expect('hello\r\n')
+print('ok.')
+
+errors = 0
+try:
+  # Apply the live patch.
+  ret = subprocess.run([trigger, str(child.pid),
+                        'libblocked_livepatch1.ulp'], timeout=1)
+  if ret.returncode:
+    print('Failed to apply livepatch #1 for libblocked')
+    errors = 1
+except subprocess.TimeoutExpired:
+  print('Deadlock reached when appling livepatch', end='')
+  print(' - AS-Unsafe conversion left untested')
+  # The deadlock test (tests/deadlock) has a far greater chance of
+  # detecting deadlocks during the application of live-patches, so
+  # return 77 to report that this test case was unable to detect the
+  # AS-Unsafe conversion.
+  errors = 77
+else:
+  # Send signals to the process which will cause the live-patched
+  # function to be called from the context of a signal-handler. If the
+  # live-patched function became AS-Unsafe, it might deadlock. Since it
+  # is not guaranteed to deadlock every time, loop it an arbitrarily
+  # chosen number of times
+  print('Testing that calling the live-patched function ', end='')
+  print('from a signal handler does not deadlock: ')
+  for attempt in range(1000):
+    print('\r' + str(attempt), end='')
+    child.kill(signal.SIGUSR1)
+    index = child.expect(['hello_world\r\n', pexpect.TIMEOUT])
+    if index == 1:
+      errors = 1
+      break
+  if index == 0:
+    print(' - ok')
+  else:
+    print(' - deadlock reached (AS-Unsafe conversion)')
+finally:
+  # Always kill the child process
+  child.close(force=True)
+
+# Kill the child process and exit
+child.close(force=True)
+exit(errors)
