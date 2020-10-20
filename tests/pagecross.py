4422fea7d7e354db1f6aab2781fb554c206cb216
Gabriel F. T. Gomes <gagomes@suse.de>
2020-10-20 12:58:48 -0300

    Test patching of functions near page boundaries
    A live patch is composed of a DSO and a metadata file. The metadata file
    contains information about the to-be-patched library and a list of
    to-be-patched functions. During the application of the live patch,
    libpulp iterates over this list, adding control information to internal
    data structures (detours), and patching the function prologue.
    
    However, when a function resides near a memory page boundary, the live
    patching process fails silently, and libpulp looses control of the
    target process, thus stopping the patching process altogether, and
    leaving the remaining functions in the list unattended.
    
    This patch adds a test cases that demonstrates the problem.

---
+++ tests/pagecross.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+#   libpulp - User-space Livepatching Library
+#
+#   Copyright (C) 2020 SUSE Software Solutions GmbH
+#
+#   This file is part of libpulp.
+#
+#   libpulp is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU Lesser General Public
+#   License as published by the Free Software Foundation; either
+#   version 2.1 of the License, or (at your option) any later version.
+#
+#   libpulp is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with libpulp.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import pexpect
+import re
+import signal
+import subprocess
+import sys
+
+# ULP tools location
+builddir = os.getcwd()
+trigger = builddir + '/../tools/trigger/ulp_trigger'
+preload = {'LD_PRELOAD': builddir + '/../lib/.libs/libpulp.so'}
+
+# Test case name
+testname = os.path.splitext(sys.argv[0])
+testname = os.path.basename(testname[0])
+
+# Start the test program and check default behavior
+child = pexpect.spawn('./' + testname, timeout=1, env=preload,
+                      encoding='utf-8')
+child.logfile = sys.stdout
+
+# Wait for the test program to be ready
+child.expect('Waiting for input.\r\n')
+
+# Apply live patch and check for new behavior
+ret = subprocess.run([trigger, str(child.pid),
+                      'libpagecross_livepatch1.ulp'])
+if ret.returncode:
+  print('Failed to apply livepatch #1 for libpagecross')
+  exit(1)
+
+child.sendline('')
+index = child.expect(['default\r\n', 'patched\r\n']);
+if index == 0:
+  print('Patch not applied correctly')
+  exit(1)
+
+# Kill the child process and exit
+child.close(force=True)
+exit(0)
