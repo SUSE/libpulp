d82e09a596b56ba938e3a6ba8aa9fd103198299a
Gabriel F. T. Gomes <gagomes@suse.de>
2020-12-10 18:33:14 -0300

    Add test case for syscall restarting
    When a thread that is in the middle of a syscall receives a signal, the
    syscall gets interrupted. On some occasions, such as when there are no
    handlers registered for the signal, or when the SA_RESTART flag is in
    effect (see sigaction(2)), the kernel arranges an automatic restarting
    of the syscall to happen when the thread is rescheduled, and it does so
    by subtracting one instruction from the program counter.
    
    Since attaching to processes causes a stopping signal to be sent to the
    attached thread, libpulp's tools trigger this subtraction mechanism.
    Even when libpulp explicitly and intentionally modifies the program
    counter to somewhere completely different (to execute live patching
    routines), the subtraction happens. Thus, in order to avoid execution of
    stray instructions, live patching routines are preceded by some nops.
    
    On the one hand, libpulp works correctly. On the other hand, the kernel
    is unaware that libpulp tampered with the program counter, so it cannot
    be sure that the syscall instruction was indeed re-executed. A problem
    would happen if the kernel assumed that the program counter subtraction
    always led to the restarting of the syscall (as if a tracer application,
    like gdb, would never hijack a thread), then never did the subtracting
    again. Fortunately, it doesn't assume that. Instead, it relies on the
    contents of the register used to return values (%rax on x86_64), to
    determine, again and again, if the syscall needs restarting, for
    instance, on kernel's arch/x86/kernel/signal.c:
    
      switch (syscall_get_error(current, regs)) {
        case -ERESTARTNOHAND:
        case -ERESTARTSYS:
        case -ERESTARTNOINTR:
          regs->ax = regs->orig_ax;
          regs->ip -= 2;
          break;
    
    where syscall_get_error(current, regs) returns the contents of %rax.
    
    This block of code gets executed before the kernel sends the thread back
    to userspace, but only after an attached tracer program has had a chance
    to play with the registers. In libpulp's case, that means that, after
    coming back from a live patching routine, the contents of %rax will have
    the result from that routine. However, before detaching from the thread,
    libpulp restores the context to the same it had when attaching,
    including %rax,, which re-triggers the subtraction mechanism, as
    exemplified below:
    
      1. Start the target program
    
        $ LD_PRELOAD=lib/.libs/libpulp.so tests/syscall_restart &
        [1] 1729
    
        (now the program is waiting for input (read syscall))
    
      2. Attaching to it (with a modified version of ulp_check that prints
         extra info) would reveal that the contents of %rax are -512
         (-ERESTARTSYS):
    
        ulp:   context.rax      : fffffffffffffe00
    
      3. After executing live patching routines, the contents of %rax
         represent the result from them (in the case of ulp_check, whether
         the live patch has benn applied or not):
    
        ulp:   curr.context.rax      : 0
    
      4. Before detaching, ulp_check restores all register:
    
        ulp:   save.context.rax      : fffffffffffffe00
    
      5. After detaching, the kernel notices that %rax equals -512
         (-512 == 0xfffffffffffffe00 == -ERESTARTSYS) and restarts the
         syscall (more precisely, it subtracts one instruction from the
         program counter, again). If %rax had not been restored, the syscall
         would not be restarted.
    
    This patch adds a test case that tests that the read syscall actually
    gets restarted after libpulp applies a live patch. It doesn't reveal any
    problems with libpulp.

---
+++ tests/syscall_restart.c
@@ -0,0 +1,81 @@
+/*
+ *  libpulp - User-space Livepatching Library
+ *
+ *  Copyright (C) 2020 SUSE Software Solutions GmbH
+ *
+ *  This file is part of libpulp.
+ *
+ *  libpulp is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  libpulp is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with libpulp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <libparameters.h>
+
+int
+main (void)
+{
+  char buffer[128] = "X";
+  ssize_t ret;
+
+  /* Signal readiness. */
+  printf ("Waiting for input.\n");
+
+  /*
+   * When a thread that is in the middle of a syscall receives a signal,
+   * the syscall gets interrupted. On some occasions, such as when there
+   * are no handlers registered for the signal, or when the SA_RESTART
+   * flag is in effect (see sigaction(2)), the kernel arranges an
+   * automatic restarting of the syscall to happen when the thread is
+   * rescheduled, and it does so by subtracting one instruction from the
+   * program counter.
+   *
+   * Notice, however, that the subtracting happens only immediately
+   * before the rescheduling, and it can't be easily detected from a
+   * tracer, such as gdb or libpulp's tools. The tracer sees a program
+   * counter that points to the next instruction after the syscall,
+   * i.e.: it doesn't see the subtraction.
+   *
+   * Now, since libpulp hijacks the target process, modifies the
+   * program counter of a selected thread, and asks the kernel to resume
+   * execution; the subtraction mechanism could be consumed, and the
+   * actual restarting of the syscall could be lost. If that happens,
+   * programs that do not handle syscall interruption would break.
+   * Besides, even if the change in behavior would not be catastrophic,
+   * it would make live patching less transparent.
+   *
+   * The following program intentionally exits in error if the read
+   * syscall is not restarted.
+   */
+  errno = 0;
+  ret = read (STDIN_FILENO, buffer, 1);
+
+  if (ret < 0) {
+    if (errno == EINTR) {
+      printf ("read syscall interrupted but not restarted.\n");
+    }
+    printf ("read syscall error code: %d", errno);
+    return 1; /* Libpulp should restart syscalls on its own. */
+  }
+  if (ret == 0) {
+    printf ("End of file.\n");
+    return 1; /* The test driver is not expected to send EOF. */
+  }
+
+  int_params (1, 2, 3, 4, 5, 6, 7, 8);
+  float_params (1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+  return 0; /* At least one byte was read. */
+}
---
+++ tests/syscall_restart.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+#   libpulp - User-space Livepatching Library
+#
+#   Copyright (C) 2020 SUSE Software Solutions GmbH
+#
+#   This file is part of libpulp.
+#
+#   libpulp is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU Lesser General Public
+#   License as published by the Free Software Foundation; either
+#   version 2.1 of the License, or (at your option) any later version.
+#
+#   libpulp is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with libpulp.  If not, see <http://www.gnu.org/licenses/>.
+
+from tests import *
+
+# Start the test program and check default behavior
+child = pexpect.spawn('./' + testname, timeout=1, env=preload,
+                      encoding='utf-8')
+child.logfile = sys.stdout
+
+child.expect('Waiting for input.')
+print('Greeting... ok.')
+
+# After printing the greeting message, the target process makes a call
+# to fgets, which calls the read syscall. Applying a live patch will
+# interrupt the syscall.
+ret = subprocess.run([trigger, str(child.pid),
+                      'libparameters_livepatch1.ulp'])
+if ret.returncode:
+  print('Failed to apply livepatch #1 for libparameters')
+  exit(1)
+
+# Send a newline, which should be received by the read syscall if it has
+# been successfully restarted by libpulp. If the syscall has not been
+# restarted, the child program will exit without printing anything.
+child.sendline('')
+child.expect('8-7-6-5-4-3-2-1\r\n');
+child.expect('10.0-9.0-8.0-7.0-6.0-5.0-4.0-3.0-2.0-1.0\r\n');
+print('Syscall restarting... ok.')
+
+# Kill the child process and exit
+child.close(force=True)
+exit(0)
