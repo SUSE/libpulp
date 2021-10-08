c324fcb2eacb263552b491a9ee730faaed3d6da2
Simon Lees <simon@simotek.net>
2021-10-08 15:33:47 +1030

    Also retreve GA (Non update) Version

--- tools/setup_package.sh
+++ tools/setup_package.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ULP_PACKAGE_NAME="openssl-1_1"
+# Use when library package and base package name are different ie libopenssl1_1 openssl-1_1
 ULP_LIB_NAME="libopenssl1_1"
 ULP_LIB_FILENAME="libcrypto.so.1.1"
 
@@ -9,17 +10,21 @@ ULP_MULTIBUILD_FILE="_multibuild.template"
 # Valid choices openSUSE / SUSE - Used for SUSE:Maintenance:XXXXX etc
 ULP_PRODUCT="SUSE"
 
-#ULP_DIST_NAME="openSUSE:Leap:15.2:Update"
-ULP_DIST_NAME="SUSE:SLE-15-SP2:Update"
+#ULP_DIST_NAME="SUSE:SLE-15-SP2"
+ULP_DIST_NAME="SUSE:SLE-15-SP2"
 
 # Comment out to disable debug prints
 ULP_DEBUG="On"
+
+# lp152 for Leap 15.2 for example - Not needed for SLE
+#ULP_PKG_SUFFIX="lp152"
 
 # Color special caracters.
 GREEN='\033[1;32m'
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
 BLUE='\033[1;34m'
+PURPLE='\033[1;35m'
 NC='\033[0m' # No Color
 
 # Check if running bash
@@ -43,9 +48,13 @@ fail () {
 
 # Debug to stderr as some functions use stdout to return info
 debug() {
-  if [[ ! -z "$DEBUG" ]]; then
+  if [[ ! -z "$ULP_DEBUG" ]]; then
     echo -e "${BLUE}DEBUG: $1${NC}\n" 1>&2;
   fi
+}
+
+info () {
+  echo -e "${PURPLE}$1${NC}\n"
 }
 
 # Check if user have the necessary tools.
@@ -55,6 +64,26 @@ for tool in osc xpath cpio rpm2cpio xxd; do
     fail "$tool not found, but is necessary by $0"
   fi
 done
+
+get_ga_version() {
+  # Get history from osc api
+  # Parse XML for versrel
+  # Only keep the last entry - sometimes stuff is broken and built multiple times
+  # Strip versrel=
+  # Strip Quotes
+  # Example api Call oosc api https://api.suse.de/build/SUSE:SLE-15-SP2:GA/standard/x86_64/openssl-1_1/_history
+
+  local package_name=$1
+  local dist_name=$2
+  local api=$3
+
+  debug "osc api $api/build/$dist_name:GA/standard/x86_64/$package_name/_history"
+  local tmp_version=$(osc api "$api/build/$dist_name:GA/standard/x86_64/$package_name/_history" |
+                   xpath -q  -e "//entry/@versrel" | tail -1 | grep -o '".*"' | sed 's/"//g')
+  local build_count=$(osc api "$api/build/$dist_name:GA/standard/x86_64/$package_name/_history" |
+                  xpath -q  -e "//entry/@bcnt" | tail -1 | grep -o '".*"' | sed 's/"//g')
+  echo "$tmp_version.$build_count"
+}
 
 get_update_version() {
   # Get history from osc api
@@ -90,25 +119,52 @@ get_rpm_filename() {
 }
 
 fetch_rpm() {
+  # Note if you have the dist NFS mount wget could be replaced with cp
+
   local tmp_dir=$1
   local product=$2
   local rpm_filename=$3
+  # Strip SUSE:
+  local dist=${4#*:}
+  local type=$5
 
-  # TODO: Also fetch non download RPM's
-  if [[ $product == "openSUSE" ]]; then
-    debug "wget -q -P \"$tmp_dir/rpms\" \"https://download.opensuse.org/update/leap/15.2/oss/x86_64/$rpm_filename\""
-    wget -q --show-progress -P "$tmp_dir/rpms" "https://download.opensuse.org/update/leap/15.2/oss/x86_64/$rpm_filename"
-    local ret=$?
-    if [[ $ret != 0 ]]; then
-      fail "wget returned $ret downloading $rpm_filename failed"
+  local dist_lower="${dist,,}"
+
+  if [[ $type == "Update" ]]; then
+    # TODO: Also fetch non download RPM's
+    if [[ $product == "openSUSE" ]]; then
+      debug "wget -q -P \"$tmp_dir/rpms\" \"https://download.opensuse.org/update/leap/$dist/oss/x86_64/$rpm_filename\""
+      wget -q --show-progress -P "$tmp_dir/rpms" "https://download.opensuse.org/update/leap/$dist/oss/x86_64/$rpm_filename"
+      local ret=$?
+      if [[ $ret != 0 ]]; then
+        fail "wget returned $ret downloading $rpm_filename failed"
+      fi
+    elif [[ $product == "SUSE" ]]; then
+      # This one uses 15-SP2 rather then SLE-15-SP2
+      debug "wget -q -P \"$tmp_dir/rpms\" \"http://download.suse.de/updates/SUSE/Updates/SLE-Module-Basesystem/${dist#*-}/x86_64/update/x86_64/$rpm_filename\""
+      wget -q --show-progress -P "$tmp_dir/rpms" "http://download.suse.de/updates/SUSE/Updates/SLE-Module-Basesystem/${dist#*-}/x86_64/update/x86_64/$rpm_filename"
+      local ret=$?
+      if [[ $ret != 0 ]]; then
+        fail "wget returned $ret downloading $rpm_filename failed"
+      fi
     fi
-
-  elif [[ $product == "SUSE" ]]; then
-    debug "wget -q -P \"$tmp_dir/rpms\" \"http://download.suse.de/updates/SUSE/Updates/SLE-Module-Basesystem/15-SP2/x86_64/update/x86_64/$rpm_filename\""
-    wget -q --show-progress -P "$tmp_dir/rpms" "http://download.suse.de/updates/SUSE/Updates/SLE-Module-Basesystem/15-SP2/x86_64/update/x86_64/$rpm_filename"
-    local ret=$?
-    if [[ $ret != 0 ]]; then
-      fail "wget returned $ret downloading $rpm_filename failed"
+  else
+    if [[ $product == "openSUSE" ]]; then
+      debug "wget -q -P \"$tmp_dir/rpms\" \"http://download.opensuse.org/distribution/leap/$dist/repo/oss/x86_64//$rpm_filename\""
+      wget -q --show-progress -P "$tmp_dir/rpms" "http://download.opensuse.org/distribution/leap/$dist/repo/oss/x86_64/$rpm_filename"
+      local ret=$?
+      if [[ $ret != 0 ]]; then
+        fail "wget returned $ret downloading $rpm_filename failed"
+      fi
+    elif [[ $product == "SUSE" ]]; then
+      # Example http://download.suse.de/full/full-sle15-sp2-x86_64/allrpms/libopenssl1_1.rpm
+      # For some reason we need sle15-sp2 rather then sle-15-sp2
+      debug "wget -q -P \"$tmp_dir/rpms\" \"http://download.suse.de/full/full-${dist_lower/sle-/sle}-x86_64/allrpms/$rpm_filename\""
+      wget -q --show-progress -P "$tmp_dir/rpms" "http://download.suse.de/full/full-${dist_lower/sle-/sle}-x86_64/allrpms/$rpm_filename"
+      local ret=$?
+      if [[ $ret != 0 ]]; then
+        fail "wget returned $ret downloading $rpm_filename failed"
+      fi
     fi
   fi
 }
@@ -170,7 +226,7 @@ is_lib_livepatcheable() {
 }
 
 # Build Target is Dist Name but with _ instead of :
-__BUILD_TARGET=${ULP_DIST_NAME//:/_}
+__BUILD_TARGET="${ULP_DIST_NAME//:/_}_Update"
 
 if [[ $ULP_PRODUCT == "openSUSE" ]]; then
 __API="https://api.opensuse.org"
@@ -180,11 +236,8 @@ else
 fail "ULP_PRODUCT is not correctly defined must be SUSE or openSUSE"
 fi
 
-# lp152 for Leap 15.2 for example - Not needed for SLE
-#__PKG_SUFFIX="lp152"
-
 if [[ -f "$ULP_MULTIBUILD_FILE" ]]; then
-  echo "$ULP_MULTIBUILD_FILE exists and therefore won't be recreated"
+  warn "$ULP_MULTIBUILD_FILE exists and therefore won't be recreated"
   __SKIP_MULTIBUILD="1"
 fi
 
@@ -195,18 +248,34 @@ fi
 __TMP_DIR=$(mktemp -d -t ulp-XXXXXXXXXX)
 mkdir -p "$__TMP_DIR/$ULP_PACKAGE_NAME-libs"
 
-for __PKG in $(osc -A $__API ls $ULP_DIST_NAME | grep "$ULP_PACKAGE_NAME."); do
+__FULL_VERSION=$(get_ga_version $ULP_PACKAGE_NAME $ULP_DIST_NAME $__API)
 
-  echo "### $__PKG ###"
+info "GA Package"
+info "Version: \"$__FULL_VERSION\""
+
+# SLE Doesn't have version in the filename but openSUSE does here
+if [[ $ULP_PRODUCT == "openSUSE" ]]; then
+  __RPM_FILENAME=$(get_rpm_filename "$ULP_LIB_NAME" "$ULP_PKG_SUFFIX" "$__FULL_VERSION")
+elif [[ $ULP_PRODUCT == "SUSE" ]]; then
+  __RPM_FILENAME="$ULP_LIB_NAME.rpm"
+fi
+fetch_rpm "$__TMP_DIR" "$ULP_PRODUCT" "$__RPM_FILENAME" "$ULP_DIST_NAME"
+
+extract_so "$__TMP_DIR" "$ULP_PACKAGE_NAME" "$__FULL_VERSION" "$ULP_LIB_FILENAME" "$ULP_LIB_NAME.rpm"
+
+# Fetch from updates
+for __PKG in $(osc -A $__API ls "$ULP_DIST_NAME:Update" | grep "$ULP_PACKAGE_NAME."); do
+
+  info "### $__PKG ###"
   __INCIDENT=${__PKG#*.}
 
   __FULL_VERSION=$(get_update_version "$__INCIDENT" "$ULP_PACKAGE_NAME" "$ULP_PRODUCT" "$__API" "$__BUILD_TARGET")
 
-  echo "Version: \"$__FULL_VERSION\""
+  info "Version: \"$__FULL_VERSION\""
 
-  __RPM_FILENAME=$(get_rpm_filename "$ULP_LIB_NAME" "$__PKG_SUFFIX" "$__FULL_VERSION")
+  __RPM_FILENAME=$(get_rpm_filename "$ULP_LIB_NAME" "$ULP_PKG_SUFFIX" "$__FULL_VERSION")
 
-  fetch_rpm $__TMP_DIR $ULP_PRODUCT $__RPM_FILENAME
+  fetch_rpm "$__TMP_DIR" "$ULP_PRODUCT" "$__RPM_FILENAME" "$ULP_DIST_NAME" "Update"
 
   if [[ ! -f "$__TMP_DIR/rpms/$__RPM_FILENAME" ]]; then
     fail "$__TMP_DIR/rpms/$__RPM_FILENAME was not downloaded correctly"
@@ -216,5 +285,7 @@ for __PKG in $(osc -A $__API ls $ULP_DIST_NAME | grep "$ULP_PACKAGE_NAME."); do
 done
 
 tar -caf "$ULP_PACKAGE_NAME-libs.tar.xz" -C "$__TMP_DIR/" "$ULP_PACKAGE_NAME-libs"
+
+ok "Succesfully created $ULP_PACKAGE_NAME-libs.tar.xz"
 
 rm -r "$__TMP_DIR"
