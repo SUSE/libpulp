6256937cef49c17c76dd53f0cbdb1d58564501b8
Simon Lees <simon@simotek.net>
2021-10-08 11:51:40 +1030

    Refactor large amounts of code into functions
    This includes the creation of the following functions
      * get_update_version
      * get_update_version
      * get_rpm_filename
      * fetch_rpm
      * extract_so
    Also adds a debug function

--- tools/setup_package.sh
+++ tools/setup_package.sh
@@ -4,7 +4,11 @@
 GREEN='\033[1;32m'
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
 NC='\033[0m' # No Color
+
+# Comment out to disable debug prints
+DEBUG="On"
 
 # Check if running bash
 if [ -z "$BASH" ]; then
@@ -25,6 +29,12 @@ fail () {
   exit 1
 }
 
+# Debug to stderr as some functions use stdout to return info
+debug() {
+  if [[ ! -z "$DEBUG" ]]; then
+    echo -e "${BLUE}DEBUG: $1${NC}\n" 1>&2;
+  fi
+}
 
 # Check if user have the necessary tools.
 for tool in osc xpath cpio rpm2cpio xxd; do
@@ -33,6 +43,86 @@ for tool in osc xpath cpio rpm2cpio xxd; do
     fail "$tool not found, but is necessary by $0"
   fi
 done
+
+get_update_version() {
+  # Get history from osc api
+  # Parse XML for versrel
+  # Only keep the last entry - sometimes stuff is broken and built multiple times
+  # Strip versrel=
+  # Strip Quotes
+  # Example api Call osc api "https://api.opensuse.org/build/openSUSE:Maintenance:16863/openSUSE_Leap_15.2_Update/x86_64/openssl-1_1.openSUSE_Leap_15.2_Update/_history"
+  local incident=$1
+  local package_name=$2
+  local product=$3
+  local api=$4
+  local build_target=$5
+
+  debug "osc api $api/build/$product:Maintenance:$incident/$build_target/x86_64/$package_name.$build_target/_history"
+  local tmp_version=$(osc api "$api/build/$product:Maintenance:$incident/$build_target/x86_64/$package_name.$build_target/_history" |
+                   xpath -q  -e "//entry/@versrel" | tail -1 | grep -o '".*"' | sed 's/"//g')
+  local build_count=$(osc api "$api/build/$product:Maintenance:$incident/$build_target/x86_64/$package_name.$build_target/_history" |
+                  xpath -q  -e "//entry/@bcnt" | tail -1 | grep -o '".*"' | sed 's/"//g')
+  echo "$tmp_version.$build_count"
+}
+
+get_rpm_filename() {
+  local lib_name=$1
+  local pkg_suffix=$2
+  local full_version=$3
+
+  if [[ -n $pkg_suffix ]]; then
+    echo "$lib_name-${full_version/"-"/"-$pkg_suffix."}.x86_64.rpm"
+  else
+    echo "$lib_name-$full_version.x86_64.rpm"
+  fi
+}
+
+fetch_rpm() {
+  local tmp_dir=$1
+  local product=$2
+  local rpm_filename=$3
+
+  # TODO: Also fetch non download RPM's
+  if [[ $product == "openSUSE" ]]; then
+    debug "wget -q -P \"$tmp_dir/rpms\" \"https://download.opensuse.org/update/leap/15.2/oss/x86_64/$rpm_filename\""
+    wget -q --show-progress -P "$tmp_dir/rpms" "https://download.opensuse.org/update/leap/15.2/oss/x86_64/$rpm_filename"
+    local ret=$?
+    if [[ $ret != 0 ]]; then
+      error "wget returned $ret downloading $rpm_filename failed"
+    fi
+
+  elif [[ $product == "SUSE" ]]; then
+    debug "wget -q -P \"$tmp_dir/rpms\" \"http://download.suse.de/updates/SUSE/Updates/SLE-Module-Basesystem/15-SP2/x86_64/update/x86_64/$rpm_filename\""
+    wget -q --show-progress -P "$tmp_dir/rpms" "http://download.suse.de/updates/SUSE/Updates/SLE-Module-Basesystem/15-SP2/x86_64/update/x86_64/$rpm_filename"
+    local ret=$?
+    if [[ $ret != 0 ]]; then
+      error "wget returned $ret downloading $rpm_filename failed"
+    fi
+  fi
+}
+
+extract_so() {
+  local tmp_dir=$1
+  local package_name=$2
+  local full_version=$3
+  local lib_filename=$4
+  local rpm_filename=$5
+
+  # Extract filename
+  mkdir -p "$tmp_dir/$package_name-libs/extract"
+  pushd "$tmp_dir/$package_name-libs/extract"
+  rpm2cpio "$tmp_dir/rpms/$rpm_filename" | cpio -idmv
+  popd
+  mkdir -p "$tmp_dir/$package_name-libs/$full_version/usr/lib64/"
+
+  cp "$tmp_dir/$package_name-libs/extract/usr/lib64/$lib_filename" "$tmp_dir/$package_name-libs/$full_version/usr/lib64/$lib_filename"
+  rm -r "$tmp_dir/$package_name-libs/extract"
+
+  is_lib_livepatcheable "$tmp_dir/$package_name-libs/$full_version/usr/lib64/$lib_filename"
+  if [ $? -ne 0 ]; then
+    warn "library $tmp_dir/$package_name-libs/$full_version/usr/lib64/$lib_filename is not livepatchable: missing NOP prologue"
+  fi
+}
 
 is_lib_livepatcheable() {
   ULP_NOPS_LEN=16
@@ -102,64 +192,29 @@ if [[ -z $__SKIP_MULTIBUILD ]]; then
   echo "<multibuild>" >> $__MULTIBUILD_FILE
 fi
 
-__tmp_dir=$(mktemp -d -t ulp-XXXXXXXXXX)
-mkdir -p "$__tmp_dir/$__PACKAGE_NAME-libs"
+__TMP_DIR=$(mktemp -d -t ulp-XXXXXXXXXX)
+mkdir -p "$__TMP_DIR/$__PACKAGE_NAME-libs"
 
 for __PKG in $(osc -A $__API ls $__DIST_NAME | grep "$__PACKAGE_NAME."); do
 
   echo "### $__PKG ###"
   __INCIDENT=${__PKG#*.}
-  # Get history from osc api
-  # Parse XML for versrel
-  # Only keep the last entry - sometimes stuff is broken and built multiple times
-  # Strip versrel=
-  # Strip Quotes
-  # Example api Call osc api "https://api.opensuse.org/build/openSUSE:Maintenance:16863/openSUSE_Leap_15.2_Update/x86_64/openssl-1_1.openSUSE_Leap_15.2_Update/_history"
-  echo "osc api $__API/build/$__PRODUCT:Maintenance:$__INCIDENT/$__BUILD_TARGET/x86_64/$__PACKAGE_NAME.$__BUILD_TARGET/_history"
-  __FULL_VERSION=$(osc api "$__API/build/$__PRODUCT:Maintenance:$__INCIDENT/$__BUILD_TARGET/x86_64/$__PACKAGE_NAME.$__BUILD_TARGET/_history" |
-                   xpath -q  -e "//entry/@versrel" | tail -1 | grep -o '".*"' | sed 's/"//g')
-  __BUILD_COUNT=$(osc api "$__API/build/$__PRODUCT:Maintenance:$__INCIDENT/$__BUILD_TARGET/x86_64/$__PACKAGE_NAME.$__BUILD_TARGET/_history" |
-                  xpath -q  -e "//entry/@bcnt" | tail -1 | grep -o '".*"' | sed 's/"//g')
-  __FULL_VERSION="$__FULL_VERSION.$__BUILD_COUNT"
-  __BUILD_VERSION=${__FULL_VERSION#*-}
 
-  if [[ -z $__PKG_SUFFIX ]]; then
-    __PKG_VERSION=${__FULL_VERSION}
-  else
-    __PKG_VERSION=${__FULL_VERSION/"-"/"-$__PKG_SUFFIX."}
-  fi
+  __FULL_VERSION=$(get_update_version "$__INCIDENT" "$__PACKAGE_NAME" "$__PRODUCT" "$__API" "$__BUILD_TARGET")
+
   echo "Version: \"$__FULL_VERSION\""
 
-  __RPM_FILENAME="$__LIB_NAME-$__PKG_VERSION.x86_64.rpm"
-  if [[ $__PRODUCT == "openSUSE" ]]; then
-    echo "wget -q -P \"$__tmp_dir/rpms\" \"https://download.opensuse.org/update/leap/15.2/oss/x86_64/$__RPM_FILENAME\""
-    wget -q --show-progress -P "$__tmp_dir/rpms" "https://download.opensuse.org/update/leap/15.2/oss/x86_64/$__RPM_FILENAME"
+  __RPM_FILENAME=$(get_rpm_filename "$__LIB_NAME" "$__PKG_SUFFIX" "$__FULL_VERSION")
 
-  elif [[ $__PRODUCT == "SUSE" ]]; then
-    echo "wget -q -P \"$__tmp_dir/rpms\" \"http://download.suse.de/updates/SUSE/Updates/SLE-Module-Basesystem/15-SP2/x86_64/update/x86_64/$__RPM_FILENAME\""
-    wget -q --show-progress -P "$__tmp_dir/rpms" "http://download.suse.de/updates/SUSE/Updates/SLE-Module-Basesystem/15-SP2/x86_64/update/x86_64/$__RPM_FILENAME"
+  fetch_rpm $__TMP_DIR $__PRODUCT $__RPM_FILENAME
+
+  if [[ ! -f "$__TMP_DIR/rpms/$__RPM_FILENAME" ]]; then
+    fail "$__TMP_DIR/rpms/$__RPM_FILENAME was not downloaded correctly"
   fi
 
-  if [[ ! -f "$__tmp_dir/rpms/$__RPM_FILENAME" ]]; then
-    fail "$__tmp_dir/rpms/$__RPM_FILENAME was not downloaded correctly"
-  fi
-
-  # Extract filename
-  mkdir -p "$__tmp_dir/$__PACKAGE_NAME-libs/extract"
-  pushd "$__tmp_dir/$__PACKAGE_NAME-libs/extract"
-  rpm2cpio "$__tmp_dir/rpms/$__RPM_FILENAME" | cpio -idmv
-  popd
-  mkdir -p "$__tmp_dir/$__PACKAGE_NAME-libs/$__FULL_VERSION/usr/lib64/"
-
-  cp "$__tmp_dir/$__PACKAGE_NAME-libs/extract/usr/lib64/$__LIB_FILENAME" "$__tmp_dir/$__PACKAGE_NAME-libs/$__FULL_VERSION/usr/lib64/$__LIB_FILENAME"
-  rm -r "$__tmp_dir/$__PACKAGE_NAME-libs/extract"
-
-  is_lib_livepatcheable "$__tmp_dir/$__PACKAGE_NAME-libs/$__FULL_VERSION/usr/lib64/$__LIB_FILENAME"
-  if [ $? -ne 0 ]; then
-    warn "library $__tmp_dir/$__PACKAGE_NAME-libs/$__FULL_VERSION/usr/lib64/$__LIB_FILENAME is not livepatchable: missing NOP prologue"
-  fi
+  extract_so "$__TMP_DIR" "$__PACKAGE_NAME" "$__FULL_VERSION" "$__LIB_FILENAME" "$__RPM_FILENAME"
 done
 
-tar -caf "$__PACKAGE_NAME-libs.tar.xz" -C "$__tmp_dir/" "$__PACKAGE_NAME-libs"
+tar -caf "$__PACKAGE_NAME-libs.tar.xz" -C "$__TMP_DIR/" "$__PACKAGE_NAME-libs"
 
-rm -r "$__tmp_dir"
+rm -r "$__TMP_DIR"
