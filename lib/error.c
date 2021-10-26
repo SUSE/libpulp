4f06584edeec78b9439f43dbf2a0b0d0c0a60d61
Giuliano Belinassi <gbelinassi@suse.com>
2021-10-26 12:12:52 -0300

    Block livepatching if fatal error is found
    If a fatal error was found when livepatching, we disable the
    livepatching mechanism instead of exiting the target process.
    
    The only case when we crash the target process is if libpulp
    initialization fails. It may be better to warn the user that the
    livepatching capabilities is not working on process startup rather than
    silently disabling it and then some sysadmin finds out that something
    went wrong weeks later.
    
    Signed-off-by: Giuliano Belinassi <gbelinassi@suse.de>

---
+++ lib/error.c
@@ -0,0 +1,113 @@
+/*
+ *  libpulp - User-space Livepatching Library
+ *
+ *  Copyright (C) 2021 SUSE Software Solutions GmbH
+ *
+ *  This file is part of libpulp.
+ *
+ *  libpulp is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  libpulp is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with libpulp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+void exit(int);
+
+/** @brief This should be the only way possible to exit the process if needed.
+ *
+ * Only use it on a catastrophic failure.
+ **/
+static inline __attribute__((noreturn)) void
+__libpulp_unique_exit_point()
+{
+  exit(1);
+}
+
+#include "error.h"
+#include "msg_queue.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/** Holds the current error state.  */
+static ulp_error_t libpulp_error_state = ULP_ERROR_NONE;
+
+ulp_error_t
+get_libpulp_error_state()
+{
+  return libpulp_error_state;
+}
+
+bool
+libpulp_is_in_error_state()
+{
+  return !(get_libpulp_error_state() == ULP_ERROR_NONE);
+}
+
+void
+libpulp_assert_func(const char *file, const char *func, int line,
+                    unsigned long expression)
+{
+  if (expression)
+    return;
+
+  msgq_push("In file = %s, function = %s, line = %d: assertion failure: %lu\n",
+            file, func, line, expression);
+  libpulp_error_state = ULP_ERROR_UNKNOWN;
+}
+
+void
+libpulp_errx_func(const char *file, const char *func, int line, int eval,
+                  const char *fmt, ...)
+{
+  va_list args;
+  va_start(args, fmt);
+  msgq_push("In file = %s, function = %s, line = %d: errx : %d ;; ", file,
+            func, line, eval);
+  msgq_push(fmt, args);
+  va_end(args);
+
+  libpulp_error_state = ULP_ERROR_UNKNOWN;
+}
+
+void
+libpulp_exit_func(const char *file, const char *func, int line, int val)
+{
+  msgq_push("In file = %s, function = %s, line = %d: exit: %d\n", file, func,
+            line, val);
+
+  libpulp_error_state = ULP_ERROR_UNKNOWN;
+}
+
+void
+libpulp_crash(const char *fmt, ...)
+{
+  va_list args;
+  va_start(args, fmt);
+  vfprintf(stderr, fmt, args);
+  va_end(args);
+
+  __libpulp_unique_exit_point();
+}
+
+void
+libpulp_crash_assert_func(const char *file, const char *func, int line,
+                          unsigned long expression)
+{
+  if (expression)
+    return;
+
+  fprintf(stderr,
+          "LIBPULP CATASTROPHIC FAILURE: In file = %s, function = %s, line = "
+          "%d: assertion failure: %lu\n",
+          file, func, line, expression);
+
+  __libpulp_unique_exit_point();
+}
