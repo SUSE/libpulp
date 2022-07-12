a66dd116d3fb3c138e3a96b0b7b76b3176f21145
Giuliano Belinassi <gbelinassi@suse.de>
2022-07-12 17:52:39 -0300

    Fix write_string
    it should take strlen + 1 as size. Also remove the `size` argument as it
    is pointless.
    
    Signed-off-by: Giuliano Belinassi <gbelinassi@suse.de>

--- tools/ptrace.h
+++ tools/ptrace.h
@@ -33,7 +33,7 @@
 
 int write_bytes(const void *buf, size_t n, int pid, Elf64_Addr addr);
 
-int write_string(const char *buffer, int pid, Elf64_Addr addr, int length);
+int write_string(const char *buffer, int pid, Elf64_Addr addr);
 
 int read_byte(char *byte, int pid, Elf64_Addr addr);
 
