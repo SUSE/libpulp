c0ec585286c855cb642ea9e0792aedae23a82886
gbelinassi <gbelinassi@suse.de>
2025-02-10 12:26:52 -0300

    Add test for ppc64le ulp_stack integrity
    Signed-off-by: gbelinassi <gbelinassi@suse.de>

---
+++ tests/librecursion2.c
@@ -0,0 +1,46 @@
+/*
+ *  libpulp - User-space Livepatching Library
+ *
+ *  Copyright (C) 2020-2021 SUSE Software Solutions GmbH
+ *
+ *  This file is part of libpulp.
+ *
+ *  libpulp is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  libpulp is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with libpulp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <librecursion2.h>
+
+long long int __attribute__((noinline))
+func_1(long long int n)
+{
+  if (n < 0)
+    return 0;
+
+  return n + func_2(n-1);
+}
+
+long long int __attribute__((noinline))
+func_2(long long int n)
+{
+  if (n < 0)
+    return 0;
+
+  return 2*n + func_1(n-1);
+}
+
+long long int __attribute__((noinline))
+recursion(long long int n)
+{
+  return func_1(n);
+}
---
+++ tests/recursion2.c
@@ -0,0 +1,54 @@
+/*
+ *  libpulp - User-space Livepatching Library
+ *
+ *  Copyright (C) 2020-2021 SUSE Software Solutions GmbH
+ *
+ *  This file is part of libpulp.
+ *
+ *  libpulp is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  libpulp is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with libpulp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <librecursion2.h>
+
+int
+main(void)
+{
+  char buffer[128];
+  int input;
+
+  /* Loop waiting for a numerical input. */
+  printf("Waiting for input.\n");
+  while (1) {
+    if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+      if (errno) {
+        perror("recursive");
+        return 1;
+      }
+      printf("Reached the end of file; quitting.\n");
+      return 0;
+    }
+    input = atoi(buffer);
+    if (input <= 0) {
+      printf("Input must be greater than zero.\n");
+      continue;
+    }
+    printf("%lld\n", recursion(input));
+  }
+
+  return 1;
+}
