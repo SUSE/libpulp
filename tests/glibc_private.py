559fdff3df54f92e8b05e467d8f59caf617e1dbe
Giuliano Belinassi <gbelinassi@suse.de>
2025-04-30 12:18:32 -0300

    Make sure libpulp.so is built without GLIBC_PRIVATE symbols
    Some symbols from glibc are not meant to be called by external
    programs, those have `GLIBC_PRIVATE` appended to them, and no other
    public symbol available to link against.
    
    One of such symbols is `__libc_reallocarray`.  But we don't need to
    interpose it, as it will eventually call realloc which is a function
    we interpose.  So eventually the lock will be held correctly.
    
    Signed-off-by: Giuliano Belinassi <gbelinassi@suse.de>

---
+++ tests/glibc_private.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+#   libpulp - User-space Livepatching Library
+#
+#   Copyright (C) 2020-2025 SUSE Software Solutions GmbH
+#
+#   This file is part of libpulp.
+#
+#   libpulp is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU Lesser General Public
+#   License as published by the Free Software Foundation; either
+#   version 2.1 of the License, or (at your option) any later version.
+#
+#   libpulp is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with libpulp.  If not, see <http://www.gnu.org/licenses/>.
+
+import re
+import testsuite
+import subprocess
+
+# Make sure libpulp is built without links to GLIBC_PRIVATE symbols.
+
+command = ['readelf', '-sW', testsuite.libpulp_path]
+try:
+  output = subprocess.check_output(command, timeout=10, stderr=subprocess.STDOUT)
+  priv = re.search('GLIBC_PRIVATE', output.decode())
+  if not priv:
+    exit(0)
+
+except subprocess.TimeoutExpired:
+  print('readelf timeout')
+  exit(77)
+
+exit(1)
